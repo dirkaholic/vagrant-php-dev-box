93e5768f8c7d8783ce6a848be1bd71ec3cbd4186
Dirk Pahl <dpahl@svz-pcn-294.intern.studivz.net>
2012-06-06 14:46:02 +0200

    First commit setting up nginx and php-fpm

---
+++ Readme.md
@@ -0,0 +1,8 @@
+Installation
+------------
+
+- Install vagrant using the installation instructions here: http://vagrantup.com/v1/docs/getting-started/index.html
+- Add a Ubuntu Precise box using the available official boxes here: https://github.com/mitchellh/vagrant/wiki/Available-Vagrant-Boxes
+- Run ```vagrant int``` for a single time
+- After running ```vagrant up``` you should have your working nginx here: http://localhost:9090/
+
---
+++ manifests/default.pp
@@ -0,0 +1,75 @@
+# Basic Puppet manifest
+
+class apt-get-update {
+
+  exec { 'apt-get update':
+    command => '/usr/bin/apt-get update'
+  }
+}
+
+class php5 {
+
+  package { "php5":
+    ensure => present,
+  }
+
+  $fpmPrerequisites = [ "php5-cli", "php5-common", "php5-suhosin" ]
+  package { $fpmPrerequisites: ensure => "installed" }
+
+  # as there was an issue in installation order we install that separatly
+  package { "php5-cgi":
+    ensure => installed,
+  }
+
+  package { "php5-fpm":
+    ensure => present,
+  }
+
+  service { "php5-fpm":
+    ensure => running,
+    require => Package["php5-fpm"],
+  }
+}
+
+class nginx {
+
+  package { "nginx":
+    ensure => present,
+  }
+
+  service { "nginx":
+    ensure => running,
+    require => Package["nginx"],
+  }
+
+  file { "/etc/nginx/sites-available/php-fpm":
+        owner  => root,
+        group  => root,
+        mode   => 664,
+        source => "/vagrant/conf/nginx/default",
+  }
+
+  file { "/etc/nginx/sites-enabled/default":
+    owner  => root,
+    ensure => link,
+    target => "/etc/nginx/sites-available/php-fpm",
+    notify => Service["nginx"],
+  }
+}
+
+class development {
+
+  $devPackages = [ "curl" ]
+  package { $devPackages: ensure => "installed" }
+
+  exec { 'install composer':
+    command => '/usr/bin/curl -s http://getcomposer.org/installer | php'
+  }
+}
+
+include apt-get-update
+include php5
+include nginx
+include development
+
+
